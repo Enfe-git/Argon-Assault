8d9c9b66ff359bb1f2d44bdc31b093529cdcf4fc
Marek Zakrzewski <mrkzakrzewski@gmail.com>
2021-06-15 20:54:24 +0100

    player now moves according to the user input
    included in quaternion for rotation to visually emphasize player movement

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,9 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+
+    [SerializeField] float controlSpeed = 30f;
+    [SerializeField] float xRange = 15f;
+    [SerializeField] float yRange = 11f;
+
+    [SerializeField] float positionPitchFactor = 3.5f;
+    [SerializeField] float positionYawFactor = 3f;
+    [SerializeField] float controlPitchFactor = -10f;
+    [SerializeField] float controlRollFactor = -30f;
+
+
+
+    float xThrow;
+    float yThrow;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -11,12 +27,38 @@ public class PlayerControls : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        float horizontalThrow =  Input.GetAxis("Horizontal");
-        float verticalThrow = Input.GetAxis("Vertical");
+    void Update() {
+        ProcessTranslation();
+        ProcessRotation();
 
-        Debug.Log("H: " + horizontalThrow);
-        Debug.Log("V: " + verticalThrow);
+    }
+
+    private void ProcessRotation() {
+
+        float pitchDueToPosition = transform.localPosition.y * -positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        
+
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    void ProcessTranslation() {
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
+
+        float xOffset = xThrow * controlSpeed * Time.deltaTime;
+        float yOffset = yThrow * controlSpeed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
+
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
