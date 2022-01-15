700a31a916e1595592d41c2ed80531eb37deb9c9
Marek Zakrzewski <mrkzakrzewski@gmail.com>
2022-01-15 20:08:52 +0000

    level now restarts on player collision

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,12 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float loadLevelDelay = 1;
     void OnCollisionEnter(Collision collision) {
-        Debug.Log(this.name + " collided with " + collision.gameObject.name);
+        StartCrashSequence();
+    }
+
+    private void StartCrashSequence() {
+        GetComponent<PlayerControls>().enabled = false;
+        Invoke("ReloadLevel", loadLevelDelay);
+    }
+
+    void ReloadLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
 }
