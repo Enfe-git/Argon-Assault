ddc2cecdff99b04180ae87816036b7eb2a483b5a
Marek Zakrzewski <mrkzakrzewski@gmail.com>
2022-02-09 20:37:22 +0000

    Processing player's score

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,11 +7,28 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit= 15;
+
+    ScoreBoard scoreBoard;
+
+    void Start() {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+
     void OnParticleCollision(GameObject other) {
+        KillEnemy();
+        ProcessHit();
+    }
+
+    void KillEnemy() {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
     }
 
-    
+    void ProcessHit() {
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
 }
